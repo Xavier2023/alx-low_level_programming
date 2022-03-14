9a5def3c8b4ffafab196877917c4a1baee6783e1
ORJINTA ANTHONY <orjintaxavier@gmail.com>
2022-03-14 07:32:10 -0700

    8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,6 +8,8 @@
 
 int main(void)
 {
-fprintf(stderr, "and that peice of art is useful\" - Dora Korpar, 2015-10-19\n");
-return(1);
+	fprint(
+	stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
+	);
+	return (1);
 }
