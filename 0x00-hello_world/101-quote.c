471a8f546bbde868fe6e929b5ab97be5db4a8b31
ORJINTA ANTHONY <orjintaxavier@gmail.com>
2022-03-14 03:04:01 -0700

    8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
+#include <unistd.h>
+
 /**
  * main - Entry point
  *
- * Return: return 1 (Error)
+ * Description: 'and that piece of art is useful' - Dora Korpar, 2015-10-19'
+ *
+ * Return: Always 1 (Success)
  */
 int main(void)
 {
-printf(stderr, "and that peice of art is useful\" - Dora Korpar, 2015-10-19\n");
+write(1, "and that peice of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return(1);
 }
