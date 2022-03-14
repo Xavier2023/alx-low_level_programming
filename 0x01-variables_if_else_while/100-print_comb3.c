042f366c88436a471c34439613f7234dc79d78b3
ORJINTA ANTHONY <orjintaxavier@gmail.com>
2022-03-14 06:51:42 -0700

    10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int x;
+	int y;
+	int z;
+
+	x = 0;
+
+	while (x < 100)
+	{
+		y = x / 10;
+		z = x % 10;
+
+		if (y < z)
+		{
+			putchar(y + '0');
+			putchar(z + '0');
+
+			if (x < 89)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+
+		x++;
+	}
+	putchar('\n');
+	return (0);
+}
