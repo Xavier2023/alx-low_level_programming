fd40d16450c0159d34af9ee9cfae2054cb8c3fbd
ORJINTA ANTHONY <orjintaxavier@gmail.com>
2022-03-14 02:54:12 -0700

    8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: return 1 (Error)
+ */
+int main(void)
+{
+printf(stderr, "and that peice of art is useful\" - Dora Korpar, 2015-10-19\n");
+}
